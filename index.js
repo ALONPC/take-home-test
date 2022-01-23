73382bfd469789a0754f6ecd8fe84e28e1422906
ALONPC <alonpc@MSI.localdomain>
2022-01-23 18:15:57 -0300

    drawer done

---
+++ index.js
@@ -0,0 +1,7 @@
+const openDrawer = () => {
+  document.getElementById("sideDrawer").style.width = "286px";
+};
+
+const closeDrawer = () => {
+  document.getElementById("sideDrawer").style.width = "0";
+};
